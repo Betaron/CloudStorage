089a7cc0e9f54f37e178ba22da0a35b3d490c6c8
Иван Агабалаев <agabalaev01@yandex.ru>
2022-05-24 06:47:34 +0300

    Configure basic api architecture
    Swager connected for debugging.
    Added serialization json controller.
    Middleware for routing, swager and https are connected.
    Activated xml comments for documentation generation.

---
+++ CloudStorage.Web/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace CloudStorage.Web;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(j =>
+        {
+            j.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {Title = "CloudStorageAPI", Version = "v1"});
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CloudStorageAPI v1");
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+}
+
